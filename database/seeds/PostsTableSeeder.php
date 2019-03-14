4bd0eebbe915967e0ddc3df5743a8175b75036ef
mohamed mahmoud <m.mahmoud@iVAS.local>
2019-03-14 17:21:47 +0200

    fix some issue

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,65 +12,37 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-
+        
 
         \DB::table('posts')->delete();
-
+        
         \DB::table('posts')->insert(array (
-            0 =>
+            0 => 
             array (
-                'id' => 2,
-                'published_date' => '2019-02-15',
+                'id' => 1,
+                'published_date' => '2019-03-06',
                 'active' => 1,
-                'patch_number' => '1245',
-                'url' => 'http://localhost:8080/ivas_template_5.6/user/content/11?op_id=2&post_id=2',
-                'content_id' => 11,
-                'operator_id' => 2,
-                'user_id' => 1,
-                'created_at' => '2019-02-25 08:45:40',
-                'updated_at' => '2019-02-25 09:51:16',
-            ),
-            1 =>
-            array (
-                'id' => 3,
-                'published_date' => '2019-02-22',
-                'active' => 0,
-                'patch_number' => '1545',
-                'url' => 'http://localhost:8080/ivas_template_5.6/user/content/11?op_id=1&post_id=3',
-                'content_id' => 11,
+                'url' => 'http://localhost/ivas_template_last/user/content/23?op_id=1&post_id=1',
+                'content_id' => 23,
                 'operator_id' => 1,
                 'user_id' => 1,
-                'created_at' => '2019-02-25 09:54:14',
-                'updated_at' => '2019-02-25 09:54:14',
+                'created_at' => '2019-03-14 08:49:45',
+                'updated_at' => '2019-03-14 08:49:45',
             ),
-            2 =>
+            1 => 
             array (
-                'id' => 4,
-                'published_date' => '2019-02-22',
-                'active' => 0,
-                'patch_number' => '1545',
-                'url' => 'http://localhost:8080/ivas_template_5.6/user/content/11?op_id=2&post_id=4',
-                'content_id' => 11,
-                'operator_id' => 2,
-                'user_id' => 1,
-                'created_at' => '2019-02-25 09:54:14',
-                'updated_at' => '2019-02-25 09:54:14',
-            ),
-            3 =>
-            array (
-                'id' => 5,
-                'published_date' => '2019-02-22',
-                'active' => 0,
-                'patch_number' => '1545',
-                'url' => 'http://localhost:8080/ivas_template_5.6/user/content/11?op_id=4&post_id=5',
-                'content_id' => 11,
+                'id' => 2,
+                'published_date' => '2019-03-06',
+                'active' => 1,
+                'url' => 'http://localhost/ivas_template_last/user/content/23?op_id=4&post_id=2',
+                'content_id' => 23,
                 'operator_id' => 4,
                 'user_id' => 1,
-                'created_at' => '2019-02-25 09:54:14',
-                'updated_at' => '2019-02-25 09:54:14',
+                'created_at' => '2019-03-14 08:49:45',
+                'updated_at' => '2019-03-14 08:49:45',
             ),
         ));
-
-
+        
+        
     }
 }
